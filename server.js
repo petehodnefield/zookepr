ef85fe15e784594d05a457e938c8128ee402d7e8
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-05-08 18:49:38 -0500

    PORT

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 const { animals } = require('./data/animals')
 
+const PORT  = process.env.PORT || 3002
 const app = express()
 
 function filterByQuery(query, animalsArray) {
@@ -49,6 +50,6 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results)
 })
-app.listen(3002, () => {
-    console.log(`API server now on port 3001!`)
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`)
 })
