f4e22ad99b5b5f683bfda4991e8e7bb036215cd1
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-05-08 18:54:46 -0500

    hope this works

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const { animals } = require('./data/animals')
 
-const PORT  = process.env.PORT || 3002
+const PORT  = process.env.PORT || 3001
 const app = express()
 
 function filterByQuery(query, animalsArray) {
